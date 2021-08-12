7d166e9a66695e69763b92a10708c22e7bbb1624
elicius cordeiro <elicius@gmail.com>
2021-08-12 17:16:52 -0400

    learning and implementing how to get values from input element without submit botton with JS

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 
+//creat
+// const test = document.querySelector('[class=tC]');
+// function toInput() {
+//     const newInput = document.createElement('input')
+//     Object.assign(newInput, {
+//         className: 'input-custom',
+//         type: 'text',
+//         placeholder: '0',
+//     })
 
-function toInput(){
-    const newInput = document.createElement('input')
-    Object.assign(newInput, {
-        className: 'input-custom',
-        type: 'text',
-        placeholder: '0',
-        test: ''
-    })
+//     newInput.setAttribute('test', '')
+//     test.replaceWith(newInput)
 
-    test.replaceWith(newInput)
-    test.getElementsByClassName('input-custom').value = 0
-};
+// };
 
-const test = document.querySelector('[tip-custom]');
-test.onclick = toInput
+const tipPercentage = document.querySelector("[class=tip_grid]", "div", "input");
+tipPercentage.onclick = e => {
+    const con = e.target.className;
+    if (con === 't') {
+       return console.log(e.target.innerHTML);
+    } else if (con === 'input-custom') {
+        const regex = new RegExp(/^[1-9][0-9]?$|^100$/)
+        console.log(e.target.value)
+        // if(regex.test(tipPercentage.onkeyup = keyup)){
+        //     console.log()          
+        // }
+    }
+}
+
+function keyup(e) {
+    let inputTextValue = e.target.value
+    return console.log(inputTextValue)
+}
+
+const billValue = document.querySelector('[bill]')
+billValue.onclick = () => {
+    billValue.onkeyup = keyup
+}
+
+const numberOfPeople = document.querySelector('[n_people]')
+numberOfPeople.onclick = () => {
+    numberOfPeople.onkeyup = keyup
+}
+
+
+
