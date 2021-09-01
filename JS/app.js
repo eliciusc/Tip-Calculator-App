3f911a9096621fd16307f74e0a84ae9441497f64
elicius cordeiro <elicius@gmail.com>
2021-09-01 15:58:36 -0400

    Comments added, error state added. A few changes in htmls class names, app is done. Maybe a few changes are necessary

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,15 @@
 //constants
 const customTipInput = document.querySelector(".input-custom");
-const customTipDiv = document.querySelectorAll(".t");
+const customTipPreDefined = document.querySelectorAll(".t");
 const bill = document.querySelector('.bill');
-const numberOfPeople = document.querySelector('[n_people]');
+const numberOfPeople = document.querySelector('.n-people');
 const totalPerPerson = document.querySelector(".display2")
 const tipPerPerson = document.querySelector('.display1')
+const spanError = document.querySelector('p span')
 const reset = document.querySelector('button')
+const regex = /^[0-9]*([0-9]|[\.])[0-9]*$/
 
 //objects
-
 const statusCalculator= {
     bill: "",
     tip: "",
@@ -16,97 +17,126 @@ const statusCalculator= {
     totalTipPerson: "$0.00",
     totalPerson: "$0.00",
     showCalc: [false, false, false],
+    cantBeZero: '',
     reset: false
-}
+};
 
-let statusCalculatorCopy = {...statusCalculator}
+let statusCalculatorCopy = {...statusCalculator};
 
 //functions
 
-function inputSet(element) {
-    let valueParsed = parseFloat(element.value)
-    if(isNaN(valueParsed) === false){
-        return valueParsed
+//Display000 the message error if user tryes to divide by 0
+function cantBeZeroError(){
+    if(inputSet(numberOfPeople) === 0){
+        spanError.innerHTML = "Can't be zero";
+        numberOfPeople.setAttribute('error', 'true');
     }else{
-        element.value = ""
-        alert("Only number allowed")
+        spanError.innerHTML = "";
+        numberOfPeople.setAttribute('error', 'false');
     }
 }
 
+//Checks if inputs are valid numbers and decimal point
+function inputSet(element) {
+    let testRegex = regex.test(element.value);
+    let valueParsed = element.value;
+
+    //checks for repeated 0 on input
+    if((valueParsed.charAt(0) === '0') && (valueParsed.charAt(1) === '0')){
+        element.value = "0";
+    }else if((valueParsed.charAt(0) === '0') && (valueParsed.charAt(1) > '0')){
+        element.value = valueParsed.charAt(1)
+    }
+
+    if(testRegex === true){
+        return parseFloat(valueParsed);
+    }else{
+        element.value = "";
+    }
+}
+
+//Checks if pre-defined inputs were selected
 const clicked = () => 
-    document.querySelectorAll('.clicked').forEach(t => t.setAttribute("class", "t"))
+    document.querySelectorAll('.clicked').forEach(t => t.setAttribute("class", "t"));
 
+//Reset the calculator
 function resetCalc(obj){
-    clicked() 
-    statusCalculatorCopy = {...obj}
+    clicked() ;
+    statusCalculatorCopy = {...obj};
 
-    bill.value = obj.bill
-    customTipInput.value = obj.tip
-    numberOfPeople.value = obj.people
-    tipPerPerson.innerHTML = obj.totalTipPerson
-    totalPerPerson.innerHTML = obj.totalPerson
-    statusCalculatorCopy.showCalc = [false, false, false]
+    bill.value = obj.bill;
+    customTipInput.value = obj.tip;
+    numberOfPeople.value = obj.people;
+    tipPerPerson.innerHTML = obj.totalTipPerson;
+    totalPerPerson.innerHTML = obj.totalPerson;
+    spanError.innerHTML = obj.cantBeZero;
+    statusCalculatorCopy.showCalc = [false, false, false];
 
+    numberOfPeople.setAttribute('error', 'false')
     reset.setAttribute("class", "disabled")
 }
 
+//Calculates tip amount and total per person
 function calculate(obj){
     reset.setAttribute("class", "")
-    obj.reset = true
+    obj.reset = true;
 
+    //check if all inputs were inserted
     const checker = arr => arr.every(item => item === true)
     if(checker(obj.showCalc) === true){
-        obj.reset = true
-        obj.totalTipPerson = (((obj.bill * obj.tip) / 100) / obj.people).toFixed(2)
-        obj.totalPerson = (obj.bill + (obj.totalTipPerson * obj.people)).toFixed(2)
-
-        totalPerPerson.innerHTML = `$${obj.totalPerson}`
-        tipPerPerson.innerHTML = `$${obj.totalTipPerson}`
+        obj.totalTipPerson = (((obj.bill * obj.tip) / 100) / obj.people).toFixed(2);
+        obj.totalPerson = ((obj.bill + (obj.totalTipPerson * obj.people)) / obj.people ).toFixed(2);
+        
+        if(isNaN(obj.totalPerson) === true){
+            totalPerPerson.innerHTML = "$0.00";
+            tipPerPerson.innerHTML = "$0.00";
+        }else{
+            totalPerPerson.innerHTML = `$${obj.totalPerson}`;
+            tipPerPerson.innerHTML = `$${obj.totalTipPerson}`;
+        }
     }
-    
-    console.log(obj)
-
 }
 
 //Events
-
 bill.addEventListener('input', e => {
-    statusCalculatorCopy.bill = inputSet(bill)
-    statusCalculatorCopy.showCalc[0] = true
-    calculate(statusCalculatorCopy)
+    statusCalculatorCopy.bill = inputSet(bill);
+    statusCalculatorCopy.showCalc[0] = true;
+    calculate(statusCalculatorCopy);
 })
 
 customTipInput.addEventListener('input', e => {
-    clicked()
-    statusCalculatorCopy.tip = inputSet(customTipInput)
-    statusCalculatorCopy.showCalc[1] = true 
-    calculate(statusCalculatorCopy)
+    clicked();
+    statusCalculatorCopy.tip = inputSet(customTipInput);
+    statusCalculatorCopy.showCalc[1] = true ;
+    calculate(statusCalculatorCopy);
 })
 
+customTipPreDefined.forEach(item => {item.addEventListener('click', e => {
+    let temp = item.innerHTML;
+    statusCalculatorCopy.tip = parseFloat(temp.slice(0, temp.length -1));
+    statusCalculatorCopy.showCalc[1] = true;
 
-customTipDiv.forEach(item => {item.addEventListener('click', e => {
-    let temp = item.innerHTML
-    statusCalculatorCopy.tip = parseFloat(temp.slice(0, temp.length -1))
-    statusCalculatorCopy.showCalc[1] = true
-
-    clicked() 
-    if(item.className === "t")
-        item.setAttribute("class", "clicked")
-    
-    calculate(statusCalculatorCopy)
+    clicked()
+    //If a pre-defined tip was selected unselect the previous option 
+    if(item.className === "t"){
+        item.setAttribute("class", "clicked");
+        customTipInput.value= "";
+    }
+    calculate(statusCalculatorCopy);
     })
 })
 
 numberOfPeople.addEventListener('input', e => {
-    statusCalculatorCopy.people = inputSet(numberOfPeople)
-    statusCalculatorCopy.showCalc[2] = true
-    calculate(statusCalculatorCopy)
+    cantBeZeroError()
+    statusCalculatorCopy.people = inputSet(numberOfPeople);
+    statusCalculatorCopy.showCalc[2] = true;
+    calculate(statusCalculatorCopy);
 })
 
 reset.addEventListener("click", e => {
     if(statusCalculatorCopy.reset === true){
-        resetCalc(statusCalculator)
+        resetCalc(statusCalculator);
     }
 })
 
-window.onload = resetCalc(statusCalculator)
+window.onload = resetCalc(statusCalculator);
